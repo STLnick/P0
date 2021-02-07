37a8e6aa6d8e88cc79ace9ec9c49d79b61a54418
Nick <nickrray@gmail.com>
2021-02-06 21:33:32 -0600

    Include new starter header files from required functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <unistd.h>
-#include "hello.hpp"
-#include "parseCommandLineOpts.hpp"
 #include "node.hpp"
+#include "parseCommandLineOpts.hpp"
+#include "printPreorder.hpp"
+#include "printInorder.hpp"
+#include "printPostorder.hpp"
 #include "tree.hpp"
-
-using namespace std;
 
 int parseCommandLineOpts(int argc, char **argv);
 
@@ -62,19 +64,44 @@ int main(int argc, char **argv)
 
   std::cout << str << std::endl;
 
-  //fptr = fopen()
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    /* !! The 3 Main Tasks as described by Mark in Project description */
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-  /* Process Command Arguments */
-  // Here in main()...
+    /* 1. Process Command Arguments */
+    // Here in main()...
 
-  /* Build the Tree */
-  // TODO - outsideFunction()
+    /* 2. Build the Tree */
+    // TODO - outsideFunction()
 
-  /* Traverse the tree 3 different ways */
-  // TODO - outsideFunction() (x3)
+    /* 3. Traverse the tree 3 different ways */
+    // TODO - outsideFunction() (x3)
 
-  hello();
 
-  fclose(fptr);
-  return 0;
+
+
+//    node_t *root = buildTree(file);
+//    printPreorder(root);
+//    printInorder(root);
+//    printPostorder(root);
+    myFile.close();
+    return 0;
 }
+
+// TODO: Build out these required functions per Project Requirements
+/*
+ * buildTree()
+ * printInorder()   -> Process Left - Process Root - Process Right
+ * printPreorder()  -> Process root - Process children left to right
+ * printPostorder() -> Process children left to right - Process root
+ */
+
+// FROM MARK IN THE PROJECT DESCRIPTION
+//Ideas for printing tree with indentations
+//static void printPreorder(nodeType *rootP,int level) {
+//    if (rootP == NULL) return;
+//    printf("%*c%d:%-9s ", level * 2, ' ', level, NodeId.info); // assume some info printed as string
+//    printf("\n");
+//    printPreorder(rootP->left, level + 1);
+//    printPreorder(rootP->right, level + 1);
+//}
